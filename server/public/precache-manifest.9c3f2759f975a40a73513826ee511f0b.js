bfdea3f110f8f12cf27f271867101d714d0edb3c
carmelobalzano <balzanoitaliano@gmail.com>
2020-08-26 16:22:34 -0400

    updates

---
+++ server/public/precache-manifest.9c3f2759f975a40a73513826ee511f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb63c46469ae3f78f4848360aa158da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2d4f04f20a9c2c380e1",
+    "url": "/static/css/main.cb270d2b.chunk.css"
+  },
+  {
+    "revision": "78d5e94b8c9d5d582445",
+    "url": "/static/js/2.9641a8d6.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.9641a8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d4f04f20a9c2c380e1",
+    "url": "/static/js/main.04b55b3f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
