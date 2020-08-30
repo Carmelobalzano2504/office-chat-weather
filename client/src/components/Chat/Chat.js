3631b94edc9ad4104d3638f5e672484a20c83845
carmelobalzano <balzanoitaliano@gmail.com>
2020-08-30 12:09:22 -0400

    updates

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://git.heroku.com/office-chat-weather.git';
+  const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
